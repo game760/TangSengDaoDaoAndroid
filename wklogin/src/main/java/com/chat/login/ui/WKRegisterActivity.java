1003b4d1105d4fce62987f30783d258dc5e79585
game760 <134137519+game760@users.noreply.github.com>
2026-01-15 14:51:50 +0800

    不限制号码类型，只检查号码长度

--- wklogin/src/main/java/com/chat/login/ui/WKRegisterActivity.java
+++ wklogin/src/main/java/com/chat/login/ui/WKRegisterActivity.java
@@ -166,9 +166,9 @@ public class WKRegisterActivity extends WKBaseActivity<ActRegisterLayoutBinding>
         wkVBinding.getVCodeBtn.setOnClickListener(v -> {
             String phone = Objects.requireNonNull(wkVBinding.nameEt.getText()).toString();
             if (!TextUtils.isEmpty(phone)) {
-				// 不限制号码，只检查号码长度
+				// 不限制号码类型，只检查号码长度
 				 if (phone.length() <= 7) {
-                    showSingleBtnDialog("号码号位数不能少于8位");
+                    showSingleBtnDialog("号码位数不能少于8位");
                     return;
                 }
                // if (code.equals("0086") && wkVBinding.nameEt.getText().toString().length() != 11) {
